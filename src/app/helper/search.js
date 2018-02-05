aa74b6eb78643c3ebb976309dec1bd6c348e0bb2
nossbigg <chenggibson@gmail.com>
2018-02-05 13:48:15 +0800

    Tune search algo

--- src/app/helper/search.js
+++ src/app/helper/search.js
@@ -1,12 +1,13 @@
 import lunr from 'lunr';
 
 const NAME_BOOST_VALUE = 5;
-const EDIT_DISTANCE = 3;
+const EDIT_DISTANCE = 1;
 
 class Search {
   constructor(specializations) {
     this.specializations = specializations;
-    this.indexedSpecializations = this.createIndexedSpecializationsStore(specializations);
+
+    this.indexedSpecializations = this.createIndexedSpecializationsStore(this.specializations);
     this.indexer = this.createIndexer(this.specializations);
   }
 
@@ -23,25 +24,24 @@ class Search {
     `;
 
     const searchResults = this.indexer.search(searchQuery);
-    const matchedSpecializations = searchResults
+    return searchResults
       .map(match => match.ref)
       .map(specializationId => this.indexedSpecializations[specializationId]);
-
-    return matchedSpecializations;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   createIndexer(specializations) {
-    const indexer = lunr(function () {
+    // eslint-disable-next-line func-names
+    return lunr(function () {
       this.ref('id');
       this.field('name');
       this.field('description');
 
       specializations.forEach(specialization => this.add(specialization));
     });
-
-    return indexer;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   createIndexedSpecializationsStore(specializations) {
     return specializations.reduce(
       (specializationMap, specialization) =>
@@ -50,6 +50,7 @@ class Search {
     );
   }
 
+  // eslint-disable-next-line class-methods-use-this
   isEmptySearchTerm(searchTerm) {
     return searchTerm.trim().length === 0;
   }
